947fc8d63932a119cfca3a81232cd56fa7fd7ffa
bhaskar360 <bhaskar360>
2024-08-14 10:20:26 +0000

    Created  Notebook

---
+++ Training/Training_1.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC
+# MAGIC select "Excuse me Brother"
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select "Brother T-Shirt"
+
+# COMMAND ----------
+
+
